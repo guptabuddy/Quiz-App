8865d7ad51a6bc9d4e3c3879c256dd145f4df440
guptabuddy <guptabudy@gmail.com>
2025-11-09 16:43:24 +0530

    Add Questions, Answers into Quiz comp., style them, and render the Quiz comp. in the App component

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,10 +1,30 @@
 import { useState } from "react";
+import QUESTIONS from "../questions.js";
 
 export default function Quiz() {
-    const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
-    const [userAnswers, setUserAnswers] = useState([]);
+	// const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
+	const [userAnswers, setUserAnswers] = useState([]);
+	const activeQuestionIndex = userAnswers.length;
 
-    return (
-        
-    );
+	function handleSelectAnswer(selectedAnswer) {
+		setUserAnswers((prevUserAnswers) => {
+			return [...prevUserAnswers, selectedAnswer];
+		});
+	}
+	console.log(userAnswers);
+
+	return (
+		<div id="quiz">
+			<div id="questions">
+				<h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+				<ul id="answers">
+					{QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+						<li key={answer} className="answer">
+							<button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+						</li>
+					))}
+				</ul>
+			</div>
+		</div>
+	);
 }
