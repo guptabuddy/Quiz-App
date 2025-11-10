8ed8314fa6c62ecfd53415ad2d3b5e7cbcdae242
guptabuddy <guptabudy@gmail.com>
2025-11-10 15:28:59 +0530

    Style the Summary which is displayed when the Quiz Ends

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import QUESTIONS from "../questions.js";
+import quizCompleteImg from "../assets/quiz-complete.png";
 
 export default function Quiz() {
 	// const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
@@ -17,6 +18,7 @@ export default function Quiz() {
 		// In above line we are checking that the Questions are completed, if yes then we will render this below "Quiz Completed" section.
 		return (
 			<div id="summary">
+				<img src={quizCompleteImg} alt="" />
 				<h2>Quiz Completed</h2>
 			</div>
 		);
@@ -27,6 +29,7 @@ export default function Quiz() {
 	// When we have read the last Question, then in the next iteration, the activeQuestionIndex will be equal to the length of the "userAnswers" array, and that index QUESTION does not exist (as length is 1 greater than the Index), so in last it will give us an error if we put this above line before the if(activeQuestionIndex === QUESTIONS.length) condition.
 	// Because that index Question does not exist.
 	shuffledAnswers.sort(() => Math.random() - 0.5);
+	// sort method sorts the array in place, so we made a copy of the available answers options for Each question (according to the activeQuestionIndex) and then sorted it randomly.
 
 	return (
 		<div id="quiz">
