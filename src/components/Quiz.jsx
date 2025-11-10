14445d61213b3672b5beab473b6b0f6c26278f5c
guptabuddy <guptabudy@gmail.com>
2025-11-10 14:41:19 +0530

    Shuffle answers of each Question, and render different content when the Quiz ends

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -11,14 +11,30 @@ export default function Quiz() {
 			return [...prevUserAnswers, selectedAnswer];
 		});
 	}
-	console.log(userAnswers);
+	// console.log(userAnswers);
+
+	if (activeQuestionIndex === QUESTIONS.length) {
+		// In above line we are checking that the Questions are completed, if yes then we will render this below "Quiz Completed" section.
+		return (
+			<div id="summary">
+				<h2>Quiz Completed</h2>
+			</div>
+		);
+	}
+
+	// This below line is going to make a copy of the available answers options for Each question (according to the activeQuestionIndex)
+	const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+	// When we have read the last Question, then in the next iteration, the activeQuestionIndex will be equal to the length of the "userAnswers" array, and that index QUESTION does not exist (as length is 1 greater than the Index), so in last it will give us an error if we put this above line before the if(activeQuestionIndex === QUESTIONS.length) condition.
+	// Because that index Question does not exist.
+	shuffledAnswers.sort(() => Math.random() - 0.5);
 
 	return (
 		<div id="quiz">
 			<div id="questions">
 				<h2>{QUESTIONS[activeQuestionIndex].text}</h2>
 				<ul id="answers">
-					{QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+					{/* {QUESTIONS[activeQuestionIndex].answers.map((answer) => ( */}
+					{shuffledAnswers.map((answer) => (
 						<li key={answer} className="answer">
 							<button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
 						</li>
