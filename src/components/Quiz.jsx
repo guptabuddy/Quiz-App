bfa97066c96f2af816312924edfdaabec8ba6acb
guptabuddy <guptabudy@gmail.com>
2025-11-14 15:00:23 +0530

    Resolve issue of shuffling answers, and issue of same key prop value to 2 components, by making a separate component

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,7 @@
 import { useState, useCallback } from "react";
 import QUESTIONS from "../questions.js";
 import quizCompleteImg from "../assets/quiz-complete.png";
-import QuestionTimer from "./QuestionTimer.jsx";
-import Answers from "./Answers.jsx";
+import Question from "./Question.jsx";
 
 export default function Quiz() {
 	// const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
@@ -50,17 +49,15 @@ export default function Quiz() {
 
 	return (
 		<div id="quiz">
-			<div id="question">
-				<QuestionTimer timeout={10000} onTimeout={handleSkipAnswer} key={activeQuestionIndex} />
-				<h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-				<Answers
-					selectedAnswer={userAnswers[userAnswers.length - 1]}
-					answers={QUESTIONS[activeQuestionIndex].answers}
-					onSelect={handleSelectAnswer}
-					answerState={answerState}
-					key={activeQuestionIndex}
-				/>
-			</div>
+			<Question
+				key={activeQuestionIndex}
+				questionText={QUESTIONS[activeQuestionIndex].text}
+				answers={QUESTIONS[activeQuestionIndex].answers}
+				selectedAnswer={userAnswers[userAnswers.length - 1]}
+				answerState={answerState}
+				onSelectAnswer={handleSelectAnswer}
+				onSkipAnswer={handleSkipAnswer}
+			/>
 		</div>
 	);
 }
